04934ff3872dd276c65372857fa0dcffc7dfeda7
Griffin Jarmin <gajarmin@vt.edu>
2015-08-02 16:53:43 -0700

    structural update

--- RNN.py
+++ RNN.py
@@ -20,7 +20,6 @@ class RNN(object):
 		self.params = [self.Uf, self.Vf, self.Wf, self.Wo]
 
 		def time_step(char, prev_hidden):
-			print(prev_hidden.type)
 			h_t = self.activation(T.dot(T.dot(self.Wf, char)*T.outer(self.Vf, self.Uf),prev_hidden))
 			y_t = T.dot(h_t, self.Wo)
 
@@ -29,39 +28,73 @@ class RNN(object):
 		[self.h, self.y_pred], _ = theano.scan(time_step,
 										sequences = self.x,
 										outputs_info=[self.h0, None])
-		print(self.h.type)
-		print(self.y_pred.type)
+
 		self.prob = T.nnet.softmax(self.y_pred)
-		print(self.prob.type)
 		self.y_out = T.argmax(self.prob, axis=-1)
-		print(self.y_out.type)
 		self.loss = lambda y: -T.mean(T.nnet.categorical_crossentropy(self.prob, y))
-		#self.loss = lambda y: -T.mean(T.log(self.prob)[T.arange(y.shape[0]), y])
+
+class RNN_Wrapper(object):
+	def __init__(self, data_input_file, learning_rate=0.01):
+		self.__read_data(data_input_file)		
+
+		self.learning_rate = learning_rate
+
+		self.y = T.imatrix()
+		self.x = T.matrix()
+
+		self.RNN = RNN(vocab_size=len(self.vocab_map), x=self.x)
+		self.predict_prob = theano.function(inputs=[self.x],
+											outputs=self.RNN.prob)
+		self.predict = theano.function(inputs=[self.x],
+										outputs=self.RNN.y_out)
+
+		self.cost = self.RNN.loss(self.y)
+
+		self.gparams = [T.grad(self.cost, param) for param in self.RNN.params]
+
+		self.updates = [(param, param + self.learning_rate * gparam) for param, gparam in zip(self.RNN.params, self.gparams)]
+
+		self.get_cost = theano.function(inputs=[self.x, self.y],
+										outputs=self.cost,
+										updates=self.updates)
+
+	def __read_data(self, data_input_file):
+		self.create_vocab_mappings(data_input_file)
+		#f = open(data_input_file, 'r')
 
 
-def create_vocab_mapping(file):
-    vocab_mapping = {}
-    i = 0
-    with open(file, 'r') as f:
-        while True:
-            char = f.read(1)
-            if not char:
-                break
-            else:
-                if char not in vocab_mapping:
-                    vocab_mapping[char] = i
-                    i += 1
-    return vocab_mapping
+	def create_vocab_mappings(self, data_input_file):
+	    #create mapping from char -> int
+	    vocab_mapping = {}
+	    i = 0
+	    with open(data_input_file, 'r') as f:
+	        while True:
+	            char = f.read(1)
+	            if not char:
+	                break
+	            else:
+	                if char not in vocab_mapping:
+	                    vocab_mapping[char] = i
+	                    i += 1
+	    self.vocab_map = vocab_mapping
+
+	    #create mapping from int -> char
+	    self.int_map = [None]*len(self.vocab_map)
+	    for char, num in self.vocab_map.iteritems():
+	    	self.int_map[num] = char
 
 
-def one_hot(v_map, char, dtype=theano.config.floatX):
-    i = v_map[char]
-    tmp = np.zeros(len(v_map), dtype=dtype)
-    tmp[i] = 1
-    return tmp
 
-def seq_2_mat(chars, vocab_map, dtype=theano.config.floatX):
-    return np.matrix(map(lambda x: one_hot(vocab_map, x, dtype=dtype), chars))
+
+
+	def one_hot(self, char, dtype=theano.config.floatX):
+	    i = self.vocab_map[char]
+	    tmp = np.zeros(len(self.vocab_map), dtype=dtype)
+	    tmp[i] = 1
+	    return tmp
+
+	def seq_2_mat(self, chars, dtype=theano.config.floatX):
+	    return np.matrix(map(lambda x: self.one_hot(x, dtype=dtype), chars))
 
 
 
